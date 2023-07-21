34b9af4812496467b2bcabcb14dc04a45aea29ea
KPajak409 <pajak.karol414@gmail.com>
2023-07-21 17:32:35 +0200

    MongoDb changes

--- MongoDb.py
+++ MongoDb.py
@@ -6,10 +6,3 @@ class MongoDb:
         client = pymongo.MongoClient(url)
         db = client['Lotto']
         self.scratch_cards = db['scratch_cards']
-
-    def insert_one(self, scratch_card):
-        x = self.scratch_cards.insert_one(scratch_card)
-        return x.inserted_id
-
-    def find_one(self, filter):
-        res = self.scratch_cards.find_one(filter)
